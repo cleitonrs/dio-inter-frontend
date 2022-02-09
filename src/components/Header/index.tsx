ca60006a6f1a10d0dccc8b29ea7813dc04193c53
cleitonrs <rscleitonrs@gmail.com>
2022-02-09 10:34:00 -0300

    Header done

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,17 @@
 import {HeaderContainer, HeaderWrapper, UserInfo} from './styles';
+import { useNavigate } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
 import UserCircle from '../UserCircle';
 
 import logoInter from '../../assets/images/Inter-orange.png';
-import { useNavigate } from 'react-router-dom';
 
 
 const Header = () => {
    
     const navigate = useNavigate();
+    const {user} = useAuth()
+
+    const initials = user.firstName.substring(0,1)+user.lastName.substring(0,1)
 
     const handleLogoff = () => {
         navigate('/')
@@ -17,10 +21,10 @@ const Header = () => {
             <HeaderWrapper>
                    <img src={logoInter} width={172} height={61} alt="logo inter" />
                 <UserInfo>
-                  <UserCircle initials="PF" />
+                  <UserCircle initials={initials} />
                   <div>
-                      <p>Olá, <span className="primary-color font-bold">Pablo</span></p>
-                      <strong>22001123-1</strong><br/>
+                      <p>Olá, <span className="primary-color font-bold">{user.firstName} {user.lastName}</span></p>
+                      <strong>{user.accountNumber} {user.accountDigit}</strong><br/>
                       <a href="/" onClick={handleLogoff}>Sair</a>
                   </div>
                 </UserInfo>
